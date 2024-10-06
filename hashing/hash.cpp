d475249be9166301a6c89e58df4de1043ca9693a
NEW-BORN <manavbhullar341@gmail.com>
2024-10-06 20:24:03 +0530

    Fix hash function bug in hash.cpp

--- hashing/hash.cpp
+++ hashing/hash.cpp
@@ -30,7 +30,8 @@ class input{
         int max_array_size= arr[max] + 1;
         int hash[max_array_size] = {0}
         for (int i = 0 ; i < n ; i++){
-            
+            int temp  = arr[i];
+            hash[temp] = hash[temp] + 1;
         }
     }
-}
+};
